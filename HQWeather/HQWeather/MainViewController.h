3f546bde474a80df3259a545b5e6a6ad30be73b8
geeeek <happy0818hong@gmail.com>
2016-07-05 09:40:01 +0800

    commit
    添加了天气APP

---
+++ HQWeather/HQWeather/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQWeather
+//
+//  Created by 胡杨科技 on 16/6/22.
+//  Copyright © 2016年 胡杨网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ HQWeather/HQWeather/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HQWeather
+//
+//  Created by 胡杨科技 on 16/6/22.
+//  Copyright © 2016年 胡杨网络. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
