d39102dc7f833360b8e787e053fe23b89866aea8
geeeek <happy0818hong@gmail.com>
2016-08-15 09:30:30 +0800

    commit
    增加了一个新的

---
+++ YiYuanYunGou-master/XBFramework/XBUi/XBBaseVC/XBBaseVC.m
@@ -0,0 +1,128 @@
+//
+//  XBBaseVC.m
+//  ZJOL
+//
+//  Created by Peter Jin (https://github.com/JxbSir) on  15/1/6.
+//  Copyright (c) 2015年 PeterJin.   Email:i@jxb.name      All rights reserved.
+//
+
+#import "XBBaseVC.h"
+
+#import <objc/runtime.h>
+
+#import "UIColor+hexColor.h"
+
+static char *btnClickAction;
+
+@implementation XBBaseVC
+@synthesize showBackBtn;
+
+- (id)init {
+    if (self = [super init]) {
+//      延伸视图包含不包含不透明的Bar，用来指定导航栏是透明的还是不透明的
+        if ([self respondsToSelector:@selector(setExtendedLayoutIncludesOpaqueBars:)])
+        {
+            self.extendedLayoutIncludesOpaqueBars = NO;
+        }
+//      是否是四周延伸，UIRectEdgeAll全屏布局
+        if([self respondsToSelector:@selector(setEdgesForExtendedLayout:)])
+        {
+            self.edgesForExtendedLayout = UIRectEdgeNone;
+        }
+//      我们从一个界面A present另一个全屏界面B时，status Bar 的外观控制被转交给被B
+        if([self respondsToSelector:@selector(setModalPresentationCapturesStatusBarAppearance:)])
+        {
+            self.modalPresentationCapturesStatusBarAppearance = YES;
+        }
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    CGRect rect = CGRectMake(0, 0, 1, 1);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context,
+                                   [NAVBAR_COLOR CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage * imge = [[UIImage alloc] init];
+    imge = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    [self.navigationController.navigationBar setBackgroundImage:imge forBarMetrics:UIBarMetricsDefault];
+    
+    NSDictionary * dict=[NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName];
+    self.navigationController.navigationBar.titleTextAttributes = dict;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -actionCustomLeftBtnWithNrlImage
+- (void)actionCustomLeftBtnWithNrlImage:(NSString *)nrlImage htlImage:(NSString *)hltImage
+                                  title:(NSString *)title
+                                 action:(void(^)())btnClickBlock {
+    self.navLeftBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.navLeftBtn setBackgroundColor:[UIColor clearColor]];
+    objc_setAssociatedObject(self.navLeftBtn, &btnClickAction, btnClickBlock, OBJC_ASSOCIATION_COPY);
+    [self actionCustomNavBtn:self.navLeftBtn nrlImage:nrlImage htlImage:hltImage title:title];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.navLeftBtn];
+}
+
+#pragma mark -actionCustomRightBtnWithNrlImage
+- (void)actionCustomRightBtnWithNrlImage:(NSString *)nrlImage htlImage:(NSString *)hltImage
+                                   title:(NSString *)title
+                                  action:(void(^)())btnClickBlock {
+    self.navRightBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    objc_setAssociatedObject(self.navRightBtn, &btnClickAction, btnClickBlock, OBJC_ASSOCIATION_COPY);
+    [self actionCustomNavBtn:self.navRightBtn nrlImage:nrlImage htlImage:hltImage title:title];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.navRightBtn];
+}
+
+#pragma mark -actionCustomNavBtn
+- (void)actionCustomNavBtn:(UIButton *)btn nrlImage:(NSString *)nrlImage
+                  htlImage:(NSString *)hltImage
+                     title:(NSString *)title {
+    [btn setImage:[UIImage imageNamed:nrlImage] forState:UIControlStateNormal];
+    if (hltImage) {
+        [btn setImage:[UIImage imageNamed:hltImage] forState:UIControlStateHighlighted];
+    } else {
+        [btn setImage:[UIImage imageNamed:nrlImage] forState:UIControlStateNormal];
+    }
+    if (title) {
+        btn.titleLabel.font = [UIFont boldSystemFontOfSize:16.];
+        [btn setTitle:title forState:UIControlStateNormal];
+        [btn setTitle:title forState:UIControlStateHighlighted];
+        [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+    }
+    [btn sizeToFit];
+    [btn addTarget:self action:@selector(actionBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+#pragma mark -actionBtnClick
+- (void)actionBtnClick:(UIButton *)btn {
+    void (^btnClickBlock) (void) = objc_getAssociatedObject(btn, &btnClickAction);
+    btnClickBlock();
+}
+
+#pragma mark -getter or setter
+- (void)setItemTitle:(NSString *)title {
+    _itemTitle = title;
+    [self.navigationItem setTitle:_itemTitle];
+}
+
+- (void)setShowBackBtn:(BOOL)showBack {
+    __weak typeof(self) wSelf = self;
+    // TODO TODO TODO - how to deal with the image resources?
+    [self actionCustomLeftBtnWithNrlImage:@"btn_back" htlImage:nil title:nil action:^{
+        [wSelf.navigationController popViewControllerAnimated:YES];
+    }];
+}
+
+
+@end
